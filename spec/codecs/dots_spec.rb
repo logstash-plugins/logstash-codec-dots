3ca22a87b92c1ee0681f8986e12f451ef96f788c
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2015-08-18 12:25:18 +0200

    add basic test for the dot encode and decode
    Fixes #3

--- spec/codecs/dots_spec.rb
+++ spec/codecs/dots_spec.rb
@@ -1,1 +1,36 @@
-require "logstash/devutils/rspec/spec_helper"
+# encoding: utf-8
+
+require_relative "../spec_helper"
+require "logstash/plugin"
+require "logstash/event"
+
+describe LogStash::Codecs::Dots do
+
+  it "should initialize without errors" do
+    klass = LogStash::Plugin.lookup("codec", "dots")
+    expect { klass.new }.to_not raise_error
+  end
+
+  describe "event encoding" do
+
+    let(:properties) { {:name => "foo" } }
+    let(:event)      { LogStash::Event.new(properties) }
+
+    it "should encode as a dot" do
+      subject.on_event do |event, data|
+        expect(data).to eq(".")
+      end
+      subject.encode(event)
+    end
+
+  end
+
+  describe "event decoding" do
+
+    it "should raise an exception" do
+      expect { subject.decode }.to raise_error
+    end
+
+  end
+
+end
